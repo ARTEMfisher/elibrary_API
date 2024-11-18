0ee394b4c8956e1cb1fe31c1a3195409a90a6a93
ARTEMfisher <artem.b_2005@mail.ru>
2024-11-19 02:03:24 +0300

    requests

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import json
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
